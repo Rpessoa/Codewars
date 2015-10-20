2dd20f189567a26f914ca36d0cb6c5a69dc5e8b0
Rpessoa <rpessoa.mobile@gmail.com>
2015-10-19 20:05:14 -0400

    add-Ternary
    Example of the same code with ternary

--- categorizeNewMember.js
+++ categorizeNewMember.js
@@ -19,3 +19,18 @@ return result;
 }
 
 openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]]);
+
+
+// or a smaller code using ternary:
+
+function openOrSenior(data){
+
+	var result = data.map(function(elem) {
+
+		return (elem[0] > 54 && elem[1] > 7) ? "Senior" : "Open";
+
+});
+
+return result;
+
+}
